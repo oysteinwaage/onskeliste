a133c0ab012ab866539de1a84a023a973b9776a8
oysteinwaage <oysteinwaage@hotmail.com>
2026-04-12 22:17:48 +0200

    endrer til å bruker .env for api nøkler

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,8 +1,8 @@
 export const FirebaseConfig = {
-  apiKey: "AIzaSyCQuZB8JnEHd_Sxm-gVxFFPyVbbDomX3HU",
-  authDomain: "wishlist-1337.firebaseapp.com",
-  databaseURL: "https://wishlist-1337.firebaseio.com",
-  projectId: "wishlist-1337",
-  storageBucket: "wishlist-1337.appspot.com",
-  messagingSenderId: "323728000348"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
 };
